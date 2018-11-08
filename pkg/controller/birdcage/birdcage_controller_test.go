eda9bb52fb246e911c6ab94af87fe2aef8322dc0
Benjamin Pineau <benjamin.pineau@datadoghq.com>
2018-11-08 21:59:10 +0100

    automated tests

--- pkg/controller/birdcage/birdcage_controller_test.go
+++ pkg/controller/birdcage/birdcage_controller_test.go
@@ -24,7 +24,7 @@ import (
 	"github.com/onsi/gomega"
 	"golang.org/x/net/context"
 	appsv1 "k8s.io/api/apps/v1"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -32,16 +32,57 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
-var c client.Client
+var (
+	c client.Client
 
-var expectedRequest = reconcile.Request{NamespacedName: types.NamespacedName{Name: "foo", Namespace: "default"}}
-var depKey = types.NamespacedName{Name: "foo-deployment", Namespace: "default"}
+	expectedRequest = reconcile.Request{NamespacedName: types.NamespacedName{Name: "foo", Namespace: "default"}}
+
+	sourceDepKey = types.NamespacedName{Name: "bar", Namespace: "default"}
+	targetDepKey = types.NamespacedName{Name: "bar-canary", Namespace: "default"}
+
+	sourceDeployment = &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      sourceDepKey.Name,
+			Namespace: sourceDepKey.Namespace,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{"deployment": instance.Name},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{Labels: map[string]string{"deployment": instance.Name}},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  "nginx",
+							Image: "nginx",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	instance = &datadoghqv1alpha1.Birdcage{
+		ObjectMeta: metav1.ObjectMeta{Name: "foo", Namespace: "default"},
+		Spec: datadoghqv1alpha1.BirdcageSpec{
+			SourceObject: datadoghqv1alpha1.BirdcageSourceObject{
+				Name:      "bar",
+				Namespace: "default",
+			},
+			TargetObject: datadoghqv1alpha1.BirdcageTargetObject{
+				Name:      "bar-canary",
+				Namespace: "default",
+				Labels:    map[string]string{"a": "b"},
+			},
+		},
+	}
+)
 
 const timeout = time.Second * 5
 
 func TestReconcile(t *testing.T) {
 	g := gomega.NewGomegaWithT(t)
-	instance := &datadoghqv1alpha1.Birdcage{ObjectMeta: metav1.ObjectMeta{Name: "foo", Namespace: "default"}}
 
 	// Setup the Manager and Controller.  Wrap the Controller Reconcile function so it writes each request to a
 	// channel when it is finished.
@@ -59,29 +100,36 @@ func TestReconcile(t *testing.T) {
 		mgrStopped.Wait()
 	}()
 
+	// Create the source (watched) deployment
+	err = c.Create(context.TODO(), sourceDeployment)
+	g.Expect(err).NotTo(gomega.HaveOccurred())
+	g.Eventually(func() error { return c.Get(context.TODO(), sourceDepKey, sourceDeployment) }, timeout).
+		Should(gomega.Succeed())
+
 	// Create the Birdcage object and expect the Reconcile and Deployment to be created
 	err = c.Create(context.TODO(), instance)
-	// The instance object may not be a valid object because it might be missing some required fields.
-	// Please modify the instance object by adding required fields and then remove the following if statement.
-	if apierrors.IsInvalid(err) {
-		t.Logf("failed to create object, got an invalid object error: %v", err)
-		return
-	}
 	g.Expect(err).NotTo(gomega.HaveOccurred())
 	defer c.Delete(context.TODO(), instance)
 	g.Eventually(requests, timeout).Should(gomega.Receive(gomega.Equal(expectedRequest)))
 
-	deploy := &appsv1.Deployment{}
-	g.Eventually(func() error { return c.Get(context.TODO(), depKey, deploy) }, timeout).
+	// Birdcage should have derived and created a new target/canary deployment
+	dep := &appsv1.Deployment{}
+	g.Eventually(func() error { return c.Get(context.TODO(), targetDepKey, dep) }, timeout).
+		Should(gomega.Succeed())
+	g.Eventually(requests, timeout).Should(gomega.Receive(gomega.Equal(expectedRequest)))
+
+	// Deleted target/canary should be re-created automaticaly
+	newdep := &appsv1.Deployment{}
+	err = c.Delete(context.TODO(), dep)
+	g.Expect(err).NotTo(gomega.HaveOccurred())
+	g.Eventually(func() error { return c.Get(context.TODO(), targetDepKey, newdep) }, timeout).
 		Should(gomega.Succeed())
 
-	// Delete the Deployment and expect Reconcile to be called for Deployment deletion
-	g.Expect(c.Delete(context.TODO(), deploy)).NotTo(gomega.HaveOccurred())
+	// Deleted birdcage custom resource should propagate
+	err = c.Delete(context.TODO(), instance)
+	g.Expect(err).NotTo(gomega.HaveOccurred())
 	g.Eventually(requests, timeout).Should(gomega.Receive(gomega.Equal(expectedRequest)))
-	g.Eventually(func() error { return c.Get(context.TODO(), depKey, deploy) }, timeout).
-		Should(gomega.Succeed())
 
 	// Manually delete Deployment since GC isn't enabled in the test control plane
-	g.Expect(c.Delete(context.TODO(), deploy)).To(gomega.Succeed())
-
+	g.Expect(c.Delete(context.TODO(), sourceDeployment)).To(gomega.Succeed())
 }
